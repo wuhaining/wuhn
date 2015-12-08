1a166d2ce1724f9464155bbb85ffc127719232b8
wuhn <765811529@qq.com>
2015-12-08 17:28:47 +0800

    smartupload

---
+++ 文档库/03-开发文档/技术点/文件上传和下载/代码/SmartUpload/src/com/wuhn/smartupload/utils/SmartUploadUtil.java
@@ -0,0 +1,21 @@
+package com.wuhn.smartupload.utils;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jspsmart.upload.SmartUpload;
+import com.jspsmart.upload.SmartUploadException;
+
+/**
+ * @author wuhn
+ * @创建时间 2015-12-08
+ * @功能 SmartUpload工具类
+ * **/
+public class SmartUploadUtil {
+	
+	
+	
+}
